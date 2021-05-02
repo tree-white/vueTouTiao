559df36810616763d198d062e7b32446dfb9ce0b
YongKai <437214404@qq.com>
2021-05-02 20:28:39 +0800

    清空

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">首页</router-link> |
-      <router-link to="/about">其他</router-link>
-    </div>
+    <!-- 这个是匹配到组件的坑(不能删除) -->
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,23 +2,12 @@
 import Vue from 'vue'
 // 导入路由
 import VueRouter from 'vue-router'
-// 导入组件 · home 是一个普通组件
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
+// 路由配置
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // 组件的懒加载，当访问 /about 页面的时候才导入加载这个 js 文件页面
-    component: () => import('../views/About.vue')
-  }
+
 ]
 
 const router = new VueRouter({
