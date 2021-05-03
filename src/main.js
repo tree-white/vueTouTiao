5a71f1899a1fc0202961726d1d6e847edff14526
YongKai <437214404@qq.com>
2021-05-04 02:49:45 +0800

    新增路由守卫

--- src/main.js
+++ src/main.js
@@ -12,14 +12,23 @@ import axios from "axios";
 // 绑定到原型对象里面，以后就可以在组件中通过 this.$axios 来调用请求方法
 // 在组件上的 this.$xxx 都是在 原型对象上的方法
 Vue.prototype.$axios = axios;
+
 // 给 axios 添加基地址，添加后请求的url就会自动拼接这个基地址
 axios.defaults.baseURL = 'http://localhost:3000';
 
 // 注册 vant 插件 （Vue.use都是用来注册插件的）
 Vue.use(Vant);
+
 // 上线环境是否提示信息（忽略）
 Vue.config.productionTip = false;
 
+// 添加路由的守卫
+// tp：代表即将要访问的页面
+// from：代表将要离开的页面
+// next：必须要调用，让中间件继续执行后面的内容。
+router.beforeEach((to, from, next) => {
+  next();
+})
 
 // 创建一个根实例
 // .$mount('#app') 相当于 el 配置，指定id为app的元素作为模版
