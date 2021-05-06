558e0f0e91b2e5c177444fc67da874b9ae21cdbf
YongKai <437214404@qq.com>
2021-05-06 16:01:03 +0800

    我的收藏多图布局完成

--- src/views/Collect.vue
+++ src/views/Collect.vue
@@ -14,6 +14,22 @@
         src="https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"
       />
     </div>
+
+    <!-- 循环的结构，大于3张图片的布局 -->
+    <div class="collect-manyImg">
+      <h4>林志玲穿透视黑纱裙米兰看秀腹部微秀腹部微秀腹部秀腹部微秀腹部秀腹部微秀腹部微隆显孕味</h4>
+      <div class="images">
+        <img
+          src="https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"
+          v-for="(item, index) in [0, 1, 2]"
+          :key="index"
+        />
+      </div>
+      <div class="manyImg-left">
+        <span>火星时报</span>
+        <span>52跟帖</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -36,22 +52,6 @@ export default {
 
   .oneImg-left {
     flex: 1;
-
-    h4 {
-      margin: 10 / 360 * 100vw 0;
-      font-weight: 400;
-      display: -webkit-box; /*将对象转为弹性盒模型展示*/
-      -webkit-box-orient: vertical; /*设置弹性盒模型子元素的排列方式*/
-      -webkit-line-clamp: 2; /*限制文本行数*/
-      overflow: hidden; /*超出隐藏*/
-    }
-
-    span {
-      display: inline-block;
-      font-size: 12px;
-      color: #aaa;
-      margin-right: 10 / 360 * 100vw;
-    }
   }
 
   img {
@@ -60,6 +60,42 @@ export default {
     width: 121 / 360 * 100vw;
     height: 75 / 360 * 100vw;
     object-fit: cover;
+  }
+}
+
+h4 {
+  margin: 10 / 360 * 100vw 0;
+  font-weight: 400;
+  display: -webkit-box; /*将对象转为弹性盒模型展示*/
+  -webkit-box-orient: vertical; /*设置弹性盒模型子元素的排列方式*/
+  -webkit-line-clamp: 2; /*限制文本行数*/
+  overflow: hidden; /*超出隐藏*/
+}
+
+span {
+  display: inline-block;
+  font-size: 12px;
+  color: #aaa;
+  margin-right: 10 / 360 * 100vw;
+}
+
+.collect-manyImg {
+  padding: 20 / 360 * 100vw;
+  border-bottom: 1px solid #eee;
+  h4 {
+      margin: 0;
+  }
+
+  .images {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10 / 360 * 100vw;
+
+    img {
+      flex: 1;
+      width: 32%;
+      object-fit: cover;
+    }
   }
 }
 </style>
