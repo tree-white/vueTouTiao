420b041dcb3171ef9d581038b452078a621daae9
YongKai <437214404@qq.com>
2021-05-07 19:19:30 +0800

    下个版本备份

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -41,8 +41,10 @@
           >
             <PostAll :arrData="list" />
           </van-list>
+
           <!-- </van-pull-refresh> -->
         </van-tab>
+        <!-- 最后跳转管理标签页面 -->
         <van-tab title="Ｖ"></van-tab>
       </van-tabs>
     </section>
@@ -64,7 +66,7 @@ export default {
 
     const { token: Authorization } =
       JSON.parse(localStorage.getItem("userInfo")) || {};
-    console.log(Authorization ? "有密令！" : "没有密令！");
+    // console.log(Authorization ? "有密令！" : "没有密令！");
 
     // 调用请求列表数据
     this.getList(Authorization);
@@ -74,50 +76,58 @@ export default {
       JSON.parse(localStorage.getItem("category")) || [];
 
     this.category = category;
-    console.log("列表栏数据:", this.category);
+    // console.log("列表栏数据:", this.category);
 
     // 有本地数据时-进行判断
     if (category) {
       // 有登录/但本地数据没关注
       if (Authorization && category[0].name !== "关注") {
-        console.log("有（本地数据）有（token）但没（关注分类）!");
+        // console.log("有（本地数据）有（token）但没（关注分类）!");
         this.getCategory(Authorization);
       }
 
       // 没登录/但本地数据有关注
       if (!Authorization && category[0].name === "关注") {
-        console.log("有（本地数据）没（token）但有（关注分类）");
+        // console.log("有（本地数据）没（token）但有（关注分类）");
         this.getCategory();
       }
 
       this.handleCategories();
-      console.log("所有栏目添加pageIndex后：", this.category);
+      //   console.log("所有栏目添加pageIndex后：", this.category);
     }
     // 没本地数据的时候则直接请求
     else {
       this.getCategory(Authorization);
-      console.log("没有本地数据");
+      //   console.log("没有本地数据");
     }
   },
 
   data() {
     return {
+      // 顶部下拉刷新组件的双向绑定
       refreshing: false,
-      // 请求的内容
+      // 列表数据
       list: [],
+      // 当前列表数据总条数，初始1，请求后会自动赋值
       total: 1,
+      // 底部刷新组件的加载判断条件
       loading: false,
+      // 底部刷新组件用来判断是否以完成所有获取
       finished: false,
-      // 本地tab栏分类的数据
+      // 本地tab栏分类的数据，又初始化请求自动赋值
       category: [],
+      // 本地tab栏默认请求的类别
       categoryId: 999,
-      // 会记录当前的值
-      active: 1,
+      // tab栏默认选中的分类
+      active: 3,
+      // 每次获取数据列表的总条数
+      pageSize: 5,
     };
   },
   methods: {
     // 循环给栏目加上 pageIndex，每个栏目都是自己的pageIndex
     handleCategories() {
+      // 给tab栏所有分类元素对象都新增一个新的pageIndex属性并赋值=1
       this.category.forEach((v) => (v.pageIndex = 1));
     },
 
@@ -129,11 +139,14 @@ export default {
         headers: { Authorization },
       }).then((resposne) => {
         // 打印输出获取逇菜单数据
-        console.log("获取栏目成功：", resposne.data.data);
+        // console.log("获取栏目成功：", resposne.data.data);
+
         // 把菜单的数据保存到本地
         localStorage.setItem("category", JSON.stringify(resposne.data));
+
         // 渲染本地数据
         this.category = resposne.data.data;
+
         // 给每个栏目添加一个 pageIndex
         this.handleCategories();
       });
@@ -145,32 +158,35 @@ export default {
       this.$axios({
         url: "/post",
         params: {
+          // 首次请求默认请求第一页
           pageIndex: 1,
+          // 请求的类别id
           category: this.categoryId,
-          pageSize: 5,
+          // 请求的数量
+          pageSize: this.pageSize,
         },
         headers: { Authorization },
       }).then((response) => {
-        console.log("请求返回的内容：", response);
-        console.log("请求返回的内容里的data：", response.data);
-        //   this.arrData = response.data.data;
-        this.list = response.data.data;
-        this.total = response.data.total;
-        console.log("总页数：", this.total);
+        // 解构出请求到的数据
+        const { total, data } = response.data;
+        // 请求到的列表数据
+        this.list = data;
+        // 请求到的列表数据总条数
+        this.total = total;
       });
     },
 
-    // 下拉触发事件
+    // 底部刷新触发事件
     onLoad() {
-      // 当前栏目下的pageIndex加1
+      // 当前栏目下的pageIndex+1
       this.category[this.active].pageIndex += 1;
 
-      // 加载下一页设定
+      // 加载下一页列表数据
       this.$axios({
         url: "/post",
         params: {
           pageIndex: this.category[this.active].pageIndex,
-          pageSize: 5,
+          pageSize: this.pageSize,
           category: this.categoryId,
         },
       }).then((response) => {
@@ -189,26 +205,37 @@ export default {
         }
       });
     },
-    onRefresh() {
-      // 清空列表数据
-      this.finished = false;
 
-      // 重新加载数据
-      // 将 loading 设置为 true，表示处于加载状态
-      this.loading = true;
-      this.onLoad();
-    },
+    // 顶部下拉刷新组件的事件
+    // onRefresh() {
+    //   // 清空列表数据
+    //   this.finished = false;
+
+    //   // 重新加载数据
+    //   // 将 loading 设置为 true，表示处于加载状态
+    //   this.loading = true;
+    //   this.onLoad();
+    // },
   },
+
+  // 监听当前点击的分类是哪一个
   watch: {
     active() {
-      //   console.log(this.active); // 测试点击了哪个tab
-      // 如果点击的是最后一个图标，跳珠啊难道栏目管理业
-      console.log(this.active);
+      // 获取当前点击的栏目索引号
+      console.log("点击了category中索引号为：", this.active);
+
+      // 如果点击的是(V)则跳转到栏目管理页面
       if (this.category && this.active === this.category.length) {
-        this.$router.push("/栏目管理业");
+        this.$router.push("/栏目管理");
+        return;
       }
+
+      // 获取当前栏目的id
+      console.log("栏目的ID：", this.category[this.active].id);
     },
   },
+
+  // 注册局部组件
   components: { PostAll },
 };
 </script>
