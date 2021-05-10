a9f6d4e6057e94015982664e7d3c958da3566c88
YongKai <437214404@qq.com>
2021-05-11 03:20:12 +0800

    PostComment - 评论列表递归渲染

---
+++ src/components/CommentFloor.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="reply-item">
+    <!-- 组件自己调用自己 -->
+    <commentFloor v-if="data.parent" :data="data.parent" />
+
+    <div class="item-top">
+      <div class="item-user">
+        <!-- <i>1</i> -->
+        <em>{{ data.user.nickname }}</em>
+        <span>{{ moment(data.create_date).fromNow() }}</span>
+      </div>
+      <span class="item-reply">回复</span>
+    </div>
+
+    <div class="item-content">{{ data.content }}</div>
+  </div>
+</template>
+
+<script>
+// moment时间处理组件
+import moment from "moment";
+
+export default {
+  // 声明当前的组件名，在当前组件里面name的值可以用于调用自己
+  name: "commentFloor",
+  props: ["data"],
+  data() {
+    return {
+      moment,
+    };
+  },
+};
+</script>
+
+<style scoped lang="less">
+.reply-item {
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 5 / 360 * 100vw;
+  font-size: 12 / 360 * 100vw;
+  padding: 2 / 360 * 100vw;
+  margin-bottom: 5 / 360 * 100vw;
+
+  .item-top {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    i {
+      padding-left: 5 / 360 * 100vw;
+    }
+
+    em {
+      margin-left: 5 / 360 * 100vw;
+      margin-right: 10 / 360 * 100vw;
+    }
+
+    span {
+      color: #999;
+    }
+  }
+
+  .item-reply {
+    margin-right: 5 / 360 * 100vw;
+  }
+
+  .item-content {
+    padding: 5 / 360 * 100vw;
+  }
+}
+</style>
--- src/views/PostComment.vue
+++ src/views/PostComment.vue
@@ -4,20 +4,21 @@
     <Titlebar title="精彩跟帖" showBack="true" />
 
     <!-- 跟帖评论列表 -->
-    <div class="comment">
+    <div class="comment" v-for="(item, index) in list" :key="index">
       <!-- 用户信息 / 回复 -->
       <div class="comment-top">
         <!-- 左边：头像/昵称/跟帖时间 -->
         <div class="user">
           <!-- 左边头像 -->
           <img
-            src="https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png"
+            :src="$axios.defaults.baseURL + item.user.head_img"
           />
 
           <!-- 右边昵称/时间 -->
           <div class="user-info">
-            <p>火星网友</p>
-            <span>2小时前</span>
+            <p>{{ item.user.nickname }}</p>
+            <!-- moment().fromNow() 显示距离到当前的时间 -->
+            <span>{{ moment(item.create_date).fromNow() }}</span>
           </div>
         </div>
 
@@ -25,17 +26,62 @@
         <span class="reply">回复</span>
       </div>
 
+      <!-- 回复列表 - 递归盒子 -->
+      <div class="reply-list">
+        <!-- item.parent有多少层数据，CommentFloor就自调用多少次 -->
+        <CommentFloor v-if="item.parent" :data="item.parent"/>
+      </div>
+
       <!-- 回复内容 -->
-      <div class="content">文章说的很有道理！</div>
-      
+      <div class="content">{{ item.content }}</div>
     </div>
   </div>
 </template>
 
 <script>
+// 头部导航组件
 import Titlebar from "@/components/Titlebar";
+// 回复评论的楼层组件
+import CommentFloor from "@/components/CommentFloor";
+// moment时间处理组件
+import moment from "moment";
+// 转换过继语言，zh-CN 就是中文
+moment.locale('zh-CN');
+
 export default {
-  components: { Titlebar },
+  components: { Titlebar, CommentFloor },
+  data() {
+    return {
+      // 激活时间组件
+      moment,
+      // 文章id
+      pid: "",
+      // 评论的列表
+      list: [],
+    };
+  },
+  mounted() {
+    // 文章的id
+    const { id } = this.$route.params;
+    // 把id保存到data
+    this.pid = id;
+
+    // 请求评论列表数据
+    this.getList();
+  },
+  methods: {
+    // 请求评论列表数据
+    getList() {
+      this.$axios({
+        url: `/post_comment/${this.pid}`,
+      }).then((res) => {
+        // data是评论的列表数组
+        const { data } = res.data;
+        // 保存到data的list数组中
+        this.list = data;
+      });
+    },
+  },
 };
 </script>
 
@@ -57,6 +103,7 @@ export default {
         width: 35 / 360 * 100vw;
         height: 35 / 360 * 100vw;
         margin-right: 5 / 360 * 100vw;
+        border-radius: 5 / 360 * 100vw;
       }
 
       .user-info {
@@ -77,6 +124,8 @@ export default {
 
   .content {
     font-size: 14 / 360 * 100vw;
+    margin-top: 10 / 360 * 100vw;
   }
 }
+
 </style>
